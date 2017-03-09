2ecf43bb57a12ad82a93aadb1e25edabd743d37d
Łukasz Sroka <sroka.dev@gmail.com>
2017-03-09 23:32:34 +0100

    threaded socket communication

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,53 @@
 pub mod user {
 
+  use std::io::Read;
+  use std::net;
+  use std::thread;
+  use std::sync::mpsc;
+
   pub struct User {
-    socket: ::std::net::TcpStream
+    socket: net::TcpStream,
+    sender: mpsc::Sender<Vec<u8>>,
   }
 
   impl User {
-    pub fn new(stream: ::std::net::TcpStream) -> User {
-      User{socket: stream}
+    pub fn new(stream: net::TcpStream, sender: mpsc::Sender<Vec<u8>>) -> User {
+      User{socket: stream,
+      sender: sender}
     }
 
-    pub fn print_addr(&self) {
-      ::std::thread::sleep(::std::time::Duration::from_millis(2000));
-      println!("{}", self.socket.peer_addr().unwrap());
+    pub fn start_reading(&self) {
+        let mut soc_clone = self.socket.try_clone().unwrap();
+        let tx = self.sender.clone();
+        thread::spawn( move || {
+          while let Some(data) = soc_clone.read_line(){
+            tx.send(data);
+          }
+        });
     }
   }
+
+  trait Reader {
+    fn read_line(&mut self) -> Option<Vec<u8>>;
+  }
+  impl Reader for net::TcpStream {
+    fn read_line(&mut self) -> Option<Vec<u8>> {
+      let mut data: Vec<u8> = Vec::new();
+      loop {
+        let mut text = [0; 20];
+        let n = self.read(&mut text).unwrap_or_default();
+        if n > 0 {
+          data.append(&mut text[0..n].to_vec());
+          if text[n-1] == 10 { // if last read character is newline
+              break;
+          }
+          continue;
+        }
+        return Option::None;
+      }
+      let _ = data.pop();
+      Option::Some(data)
+    }
+  }
+  
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate MESC;
 
 use std::net;
 use std::thread;
+use std::sync::mpsc;
 use MESC::user;
 
 fn main() {
@@ -9,18 +10,22 @@ fn main() {
 
   for stream in listener.incoming() {
     match stream {
-      Ok(stream) => {
-        handle_client(stream);
-      },
-      Err(e) => {
-
-      },
+      Ok(stream) => handle_client(stream),
+      Err(e) => panic!(e)
     }
   }
 }
 
 fn handle_client(stream: net::TcpStream) {
   thread::spawn(|| {
-    user::User::new(stream).print_addr();
+    let (tx, rx) = mpsc::channel();
+    let u = user::User::new(stream, tx);
+    u.start_reading();
+    thread::spawn(move || {
+      while let Ok(data) =  rx.recv() {
+        println!("{:?}", String::from_utf8(data).unwrap());
+      }
+      println!("closed!");
+    });
   });
 }
